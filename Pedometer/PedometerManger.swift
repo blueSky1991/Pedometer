753bc8f779696730f8f79a7f016f36a16d56dbfe
ddEfforts <m18210681011@163.com>
2017-04-14 12:20:06 +0800

    init

---
+++ Pedometer/PedometerManger.swift
@@ -0,0 +1,199 @@
+//
+//  PedometerManger.swift
+//  Pedometer
+//
+//  Created by 张东东 on 2017/4/13.
+//  Copyright © 2017年 张东东. All rights reserved.
+//
+
+import UIKit
+import HealthKit
+
+
+final class PedometerManger: NSObject {
+    
+    var healthStore : HKHealthStore?
+    
+    
+    static var shareInstance: PedometerManger {
+        
+        struct Static {
+            
+            static let instance: PedometerManger = PedometerManger()
+        }
+        
+        return Static.instance
+    }
+    
+     private override init() {}
+    
+    
+     func authorizeHealthKit(version:CGFloat , completion: @escaping (Bool,Error?) -> ()) {
+        
+         if version > 8.0 {
+            
+            if HKHealthStore.isHealthDataAvailable() {
+                
+                self.healthStore = HKHealthStore.init()
+                let write = writeCompetence()
+                let read = readCompetence()
+
+                self.healthStore?.requestAuthorization(toShare: write, read: read, completion: { (success, error) in
+                    
+                     completion(success, error)
+                    
+                })
+
+            }else{
+            
+                let error : NSError = NSError.init(domain: "com.raywenderlich.tutorials.healthkit", code: 2, userInfo: [NSLocalizedDescriptionKey : "HealthKit is not available in th is Device"])
+                
+                    completion(false,error)
+                
+                   return
+
+            }
+        
+         }else{
+            
+            let error : NSError = NSError.init(domain: "com.sdqt.healthError", code: 0, userInfo: [NSLocalizedDescriptionKey : "iOS 系统低于8.0"])
+            
+            completion(false,error)
+
+            
+        }
+    
+    }
+    
+    
+    public func getStepCount(completion: @escaping (Int,Error?) -> ()) {
+    
+        let stepCount = HKQuantityType.quantityType(forIdentifier: .stepCount)
+
+        let timeSortDescriptor = NSSortDescriptor.init(key:HKSampleSortIdentifierEndDate, ascending: false)
+        
+    
+        let query = HKSampleQuery.init(sampleType:stepCount!, predicate: predicateForSamplesToday(), limit: 0, sortDescriptors: [timeSortDescriptor]) { (query, results, error) in
+            
+            if (error != nil) {
+                completion(0,error)
+            }else{
+            
+                var  totleSteps:Double = 0.0;
+                
+                for i in 0 ..< (results?.count)! {
+                     let quantitySample =  results?[i] as! HKQuantitySample
+                     let quantity = quantitySample.quantity
+                     let heightUnit = HKUnit.count()
+                     let usersHeight = quantity.doubleValue(for:heightUnit)
+                     totleSteps += usersHeight
+                    
+                }
+                
+                
+                completion(Int(totleSteps),error)
+            }
+
+        }
+        
+        self.healthStore?.execute(query)
+        
+    }
+    
+    //获取公里数
+    public func getDistance(compltion: @escaping (_ value:Double,_ error:NSError) -> Void){
+    
+    
+        let WalkingRunning = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)
+        
+        let timeSortDescriptor = NSSortDescriptor.init(key:HKSampleSortIdentifierEndDate, ascending: false)
+        
+        
+        let query = HKSampleQuery.init(sampleType:WalkingRunning!, predicate: predicateForSamplesToday(), limit: 0, sortDescriptors: [timeSortDescriptor]) { (query, results, error) in
+            
+            if (error != nil) {
+                compltion(0,error! as NSError)
+            }else{
+                
+                var  totleSteps:Double = 0.0;
+                
+                for i in 0 ..< (results?.count)! {
+                    let quantitySample =  results?[i] as! HKQuantitySample
+                    let quantity = quantitySample.quantity
+                    let heightUnit = HKUnit.count()
+                    let usersHeight = quantity.doubleValue(for:heightUnit)
+                    totleSteps += usersHeight
+                    
+                }
+                
+                compltion(totleSteps,error! as NSError)
+            }
+            
+            
+            
+        }
+        
+        self.healthStore?.execute(query)
+
+        
+        
+    }
+    
+    func predicateForSamplesToday() -> NSPredicate {
+        
+        let calendar = Calendar.current
+
+        let set = Set.init(arrayLiteral:Calendar.Component.day,Calendar.Component.month,Calendar.Component.year)
+        
+        var components = calendar.dateComponents(set, from: Date.init())
+        
+        components.hour = 0
+        components.minute = 0
+        components.second = 0
+
+        let startDate = calendar.date(from: components)
+        let endDate = calendar.date(byAdding: Calendar.Component.day, value: 1, to: startDate!, wrappingComponents: true)
+        
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .init(rawValue: 0))
+        return predicate
+        
+        
+    }
+    
+
+    
+    
+    
+}
+
+
+extension PedometerManger{
+
+    fileprivate   func writeCompetence() -> Set<HKQuantityType> {
+    
+        let heightType = HKQuantityType.quantityType(forIdentifier: .height)
+        let bodyMassType = HKQuantityType.quantityType(forIdentifier: .bodyMass)
+        let bodyTemperatureType = HKQuantityType.quantityType(forIdentifier: .bodyTemperature)
+        let activeEnergyBurnedType = HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)
+        
+        return Set.init(arrayLiteral: heightType!,bodyMassType!,bodyTemperatureType!,activeEnergyBurnedType!)
+        
+    }
+    
+    fileprivate   func readCompetence() -> Set<HKQuantityType> {
+        let height = HKQuantityType.quantityType(forIdentifier: .height)
+        let bodyMass = HKQuantityType.quantityType(forIdentifier: .bodyMass)
+        let bodyTemperature = HKQuantityType.quantityType(forIdentifier: .bodyTemperature)
+        let stepCount = HKQuantityType.quantityType(forIdentifier: .stepCount)
+        let distanceWalkingRunning = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)
+        let activeEnergyBurned = HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)
+        return Set.init(arrayLiteral: height!,bodyMass!,bodyTemperature!,stepCount!,distanceWalkingRunning!,activeEnergyBurned!)
+    }
+    
+
+}
+
+
+
+
+
---
+++ Pedometer/ViewController.swift
@@ -0,0 +1,221 @@
+//
+//  ViewController.swift
+//  Pedometer
+//
+//  Created by 张东东 on 2017/4/13.
+//  Copyright © 2017年 张东东. All rights reserved.
+//
+
+import UIKit
+import HealthKit
+
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var showStep: UILabel!
+    
+    @IBOutlet weak var stepNumer: UITextField!
+    
+    @IBOutlet weak var addStep: UIButton!
+    
+    var manager : PedometerManger?
+    var healthStore :HKHealthStore?
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.stepNumer.delegate = self
+        self.stepNumer.borderStyle = .roundedRect
+        self.stepNumer.keyboardType = .numberPad
+        
+        
+        self.addStep.layer.borderWidth = 1
+        self.addStep.layer.borderColor = UIColor.darkGray.cgColor
+        self.addStep.layer.cornerRadius = 5
+        self.addStep.layer.masksToBounds = true
+        
+        getCompetence()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+    @IBAction func addStepNumerAction(_ sender: UIButton) {
+        
+        if (self.stepNumer.text?.isEmpty)! {
+            let alterView = UIAlertController.init(title: nil, message: "步数不能为空", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "确定", style: .default, handler:{
+                (UIAlertAction) -> Void in
+                self.stepNumer.text = ""
+            })
+            alterView.addAction(okAction)
+            self.present(alterView, animated: true, completion: nil)
+            return
+        }
+        
+        
+        
+        
+        self.stepNumer.endEditing(true)
+        let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)
+        
+        if HKHealthStore.isHealthDataAvailable() {
+                let stepQuantity = HKQuantity.init(unit: HKUnit.count(), doubleValue: Double(self.stepNumer.text!)!)
+    
+                 let stepSample = HKQuantitySample.init(type: stepType!, quantity: stepQuantity, start: Date.init(), end: Date.init())
+            
+                 self.healthStore?.save(stepSample, withCompletion: { (success, error) in
+                    
+                    
+                    if success {
+                        
+                        DispatchQueue.main.async {
+                            
+                            let alterView = UIAlertController.init(title: "提示", message: "步数已加上", preferredStyle: .alert)
+                            
+                            let okAction = UIAlertAction(title: "确定", style: .default, handler:{
+                                (UIAlertAction) -> Void in
+                                self.stepNumer.text = ""
+                                self.getStepCount()
+                            })
+                            
+                            alterView.addAction(okAction)
+                            self.present(alterView, animated: true, completion: nil)
+                            
+                            
+                            
+                        }
+
+                    
+                    }else{
+                    
+                        DispatchQueue.main.async {
+                            
+                            let alterView = UIAlertController.init(title:"", message:error.debugDescription , preferredStyle: .alert)
+                            
+                            let okAction = UIAlertAction(title: "确定", style: .default, handler:{
+                                (UIAlertAction) -> Void in
+                                
+                            })
+                            
+                            alterView.addAction(okAction)
+                            self.present(alterView, animated: true, completion: nil)
+                            
+                        }
+
+                    
+                    }
+                    
+                 })
+            
+        }
+        
+    }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.stepNumer.endEditing(true)
+    }
+
+}
+
+
+extension ViewController {
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    
+        if (textField.text?.isEmpty)! {
+            self.addStep.isEnabled = false
+        }else{
+            
+            self.addStep.isEnabled = true
+        }
+
+        return true
+    }
+
+}
+
+
+extension ViewController {
+
+    //获取权限
+     fileprivate  func getCompetence()  {
+        
+        self.manager = PedometerManger.shareInstance
+        self.healthStore = HKHealthStore.init()
+        
+        if HKHealthStore.isHealthDataAvailable(){
+            let write = writeCompetence()
+            let read = readCompetence()
+
+            self.healthStore?.requestAuthorization(toShare: write, read: read, completion: { (success, error) in
+                if success {
+                    print("success")
+                      self.getStepCount()
+                }
+                
+            })
+
+        }
+    }
+
+    
+    fileprivate   func writeCompetence() -> Set<HKQuantityType> {
+    
+
+        let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)
+    
+        return Set.init(arrayLiteral: stepType!)
+
+    }
+    
+    fileprivate   func readCompetence() -> Set<HKQuantityType> {
+        
+        
+        let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)
+        
+        return Set.init(arrayLiteral: stepType!)
+        
+    }
+
+    
+    
+    func getStepCount() {
+        
+        
+        self.manager = PedometerManger.shareInstance
+       
+        weak var weakSelf = self
+        
+        let version = ( (UIDevice.current.systemVersion) as NSString).floatValue
+
+        weakSelf?.manager?.authorizeHealthKit(version: CGFloat(version), completion:  { (success, error) in
+    
+        if success {
+            
+            weakSelf?.manager?.getStepCount(completion:{ (value, error) in
+                
+                        let strongSelf = self
+                
+                         DispatchQueue.main.async {
+                            
+                            strongSelf.showStep.text = "今日步数:"+String(value)
+                }
+        
+            })
+
+        }
+
+     })
+
+        
+    }
+    
+}
+
